4721d2f32d3c5bb79f0afd62241c7ac440409e54
Alfonso <alfonsogaitanperez@gmail.com>
2025-12-24 15:32:47 +0100

    Projects fixed, modals are next

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tecnologias } from '../known-techs/known-techs';
 
 @Component({
   selector: 'app-projects',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './projects.css',
 })
 export class Projects {
+  proyectos: Project[] = [
+    {
+      url: './thumbnails/Voltpath.jpg',
+      background_class: 'voltpath-background',
+      descripcion:''
+    },
+    {
+      url: './thumbnails/Genesis.png',
+      background_class: 'genesis-background',
+      descripcion:''
+    },
+  ]
+}
 
+
+export interface Project {
+  url: string,
+  background_class: string,
+  descripcion: string,
 }
